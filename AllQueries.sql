ff91bdd7fcc2106f8aca6817b132b1f17e5ce895
Yajat Rajput <yajatrajput41@gmail.com>
2025-08-29 22:18:45 +0530

    sql practice

--- AllQueries.sql
+++ AllQueries.sql
@@ -80,3 +80,10 @@ select c.category,count(cat.category) as accounts_count
 from categories c
 left join categorized cat on c.category=cat.category
 group by c.category;
+
+Q 1280....
+with cte as (select * from Students cross join Subjects),
+cte2 as (select student_id,subject_name,count(subject_name) as count
+from Examinations group by student_id, subject_name)
+select cte.student_id,cte.student_name,cte.subject_name, case when count is not null then count else 0 end as attended_exams
+from cte left join cte2 on cte.student_id  = cte2.student_id and cte.subject_name = cte2.subject_name order by cte.student_id,cte.subject_name;
